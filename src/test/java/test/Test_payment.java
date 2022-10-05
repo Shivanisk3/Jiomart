733a5ef1cdc5569d558e33517966472c75eef8aa
divyashinde47 <79256972+divyashinde47@users.noreply.github.com>
2022-10-05 14:37:22 +0530

    Update Test_payment.java
    Divya Shinde 2nd commit

--- src/test/java/test/Test_payment.java
+++ src/test/java/test/Test_payment.java
@@ -1,94 +1,32 @@
 package test;
-/*
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
-import pages.payment;
+//package Sample.Sample_Practice;
 
-
-public class Test_payment {
-	String url = "https://www.jiomart.com/";
-	WebDriver driver;
-	
-	payment p;
-	
-	@BeforeTest
-	  public void beforeTest() {
-		//System.setProperty("webdriver.gecko.driver", "D:\\geckodriver-v0.31.0-win64\\geckodriver.exe");
-		WebDriverManager.firefoxdriver().setup();
-		driver = new FirefoxDriver();
-		driver.get(url);
-		driver.manage().window().maximize();
-		p=new payment(driver);
-	  }
-	
-	 @Test(priority = 1)
-	  public void signIn() throws InterruptedException{
-		 p.signIn();
-	  }
-	  @Test(priority = 2)
-	  public void AddCartClick() throws InterruptedException{
-	     p.AddCartClick();
-	  }
-	  @Test(priority = 3)
-	  public void ProductAddToCart() throws InterruptedException{
-		  p.ProductAddToCart();
-	  }
-	  @Test(priority = 4)
-	  public void visibleAddToCartProduct() throws InterruptedException{
-		  p.visibleAddToCartProduct();
-	  }
-	 
-	  @Test(priority = 5)
-	  public void TotalAmount() throws InterruptedException{
-		  p.TotalAmount();
-	  }
-	  @Test(priority = 6)
-	  public void placeorderclick() throws InterruptedException{
-		  p.placeorderclick();
-	  }
-	  @Test(priority=7)
-	  public void paymentMode() {
-		  p.paymentMode();
-	  }
-	 
-	  @AfterTest
-	  public void afterTest() {
-	driver.quit();
-	  }
-}
-*/
-
-
-import java.time.Duration;
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-
-public class Test_payment {
+public class Payment2 {
 public String baseUrl = "https://www.jiomart.com/";
     String driverPath = "D:\\D downloads\\chromedriver_win321\\chromedriver.exe";
     public WebDriver driver;
  
   @BeforeTest
   public void beforeTest() {
-	  //System.setProperty("webdriver.gecko.driver", "D:\\geckodriver-v0.31.0-win64\\geckodriver.exe");
-	  WebDriverManager.firefoxdriver().setup();
-		driver = new FirefoxDriver();
-      //driver.manage().timeouts().implicitlyWait(500, TimeUnit.MILLISECONDS);
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+ System.setProperty("webdriver.chrome.driver", driverPath);
+      driver= new ChromeDriver();
+      driver.manage().timeouts().implicitlyWait(500, TimeUnit.MILLISECONDS);
   }
   @Test(priority = 1)
   public void launch() throws InterruptedException{
@@ -105,7 +43,7 @@ Thread.sleep(2000);
  driver.findElement(By.xpath("/html/body/div[1]/div[1]/header/div/div[5]/div/a[1]")).click();
  Thread.sleep(2000);
  // driver.findElement(By.id("loginfirst_mobileno")).sendKeys("8668335762");
- driver.findElement(By.id("loginfirst_mobileno")).sendKeys("9359317483");
+ driver.findElement(By.id("loginfirst_mobileno")).sendKeys("9359746720");
   driver.findElement(By.cssSelector("button.btn-signpass.arrowbtn")).click();
   Thread.sleep(25000);
   driver.findElement(By.className("btn-login")).click();
@@ -114,7 +52,7 @@ Thread.sleep(2000);
  }
   @Test(priority = 3)
   public void AddCartClick() throws InterruptedException{
-	  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
  driver.findElement(By.id("minicart_btn")).click();
  Thread.sleep(2000);
       System.out.println("Test Case 2:Add To Cart Button Clickable Successfully");
@@ -122,12 +60,11 @@ Thread.sleep(2000);
   }
   @Test(priority = 4)
   public void ProductAddToCart() throws InterruptedException{
-	  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
  WebElement r =  driver.findElement(By.id("mat-input-0"));
- driver.findElement(By.id("mat-input-0")).click();
-      //r.click();
+      r.click();
       Thread.sleep(2000);
-      r.sendKeys("soap");
+      r.sendKeys("Surfexel");
      Thread.sleep(3000);
       r.sendKeys(Keys.RETURN);
     JavascriptExecutor js=(JavascriptExecutor)driver;//casting
@@ -167,7 +104,7 @@ Thread.sleep(2000);
  System.out.println("Deliver here button clicked successfully");
   }
   @Test(priority=8)
-  public void paymentMode() throws InterruptedException {
+  public void paymentMode() throws InterruptedException, IOException {
  driver.findElement(By.cssSelector("button.btn-checkout.btn.btn_to_checkout.m-0")).click();
  Thread.sleep(2000);
  JavascriptExecutor s1=(JavascriptExecutor)driver;//casting
@@ -201,8 +138,21 @@ driver.findElement(By.linkText("Orders")).click();
 Thread.sleep(5000);
 //view detail
 driver.findElement(By.xpath("//*[@id=\"app\"]/main/div/app-orderhistory/div[1]/div/div[2]/div[2]/div/app-order-list/div/section/app-order-card[1]/div[1]/div[3]/div[2]/div/div[1]/button")).click();
+
+Thread.sleep(3000);
+TakesScreenshot scrShot=((TakesScreenshot)driver);
+
+//Call getScreenShotAs method to create image file
+File SrcFile=scrShot.getScreenshotAs(OutputType.FILE);
+
+//Copy file to Desired Location
+FileUtils.copyFile(SrcFile,new File("C:/Users/divya_shinde/eclipse-workspace/Selenium/Screenshots/Screenshot2.png"));
+
 //cancel order
-driver.findElement(By.xpath("//*[@id=\"app\"]/main/div/app-order-detail/div[1]/div[2]/div[1]/div[1]/div[2]/div/div[1]/button")).click();
+driver.findElement(By.xpath("//*[@id='app']/main/div/app-order-detail/div[1]/div[2]/div[1]/div[1]/div[2]/div/div[1]/button")).click();
+
+
+
 //no longer
 WebElement radio2 =driver.findElement(By.name("reasonselect"));
 radio2.click();
